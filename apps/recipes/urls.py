164b436e359749b53ed7256cb652b138f6b6545a
Nicholas Farias <nicholas@eiya.mx>
2021-11-12 23:39:26 -0600

    feat(#1-create_ingredient_endpoint): add ingredients viewset

--- apps/recipes/urls.py
+++ apps/recipes/urls.py
@@ -1,6 +1,7 @@
 from rest_framework.routers import SimpleRouter
-from .views import RecipesView
+from .views import (RecipesView, IngredientsView)
 
 RECIPES_ROUTER = SimpleRouter()
 
 RECIPES_ROUTER.register(r'/recipes', RecipesView, 'recipes')
+RECIPES_ROUTER.register(r'/ingredients', IngredientsView, 'ingredients')
--- apps/recipes/views.py
+++ apps/recipes/views.py
@@ -4,12 +4,24 @@ from .serializers import (RecipeSerializer, IngredientSerializer)
 from .models import (Recipe, Ingredient)
 
 class RecipesView(mixins.ListModelMixin, viewsets.GenericViewSet):
-    def get_queryset(self):
-        recipes = Recipe.objects.all()
+    """..."""
+    def get_queryset(self, search):
+        if not search:
+            recipes = Recipe.objects.all()
+        else:
+            recipes = Recipe.objects.filter(name__contains=search)
         return recipes
 
     def list(self, request):
         # Note the use of `get_queryset()` instead of `self.queryset`
-        queryset = self.get_queryset()
+        search = request.GET.get('search', None)
+        queryset = self.get_queryset(search)
         serializer = RecipeSerializer(queryset, many=True)
         return Response(serializer.data)
+
+
+class IngredientsView(mixins.ListModelMixin, viewsets.GenericViewSet):
+    """..."""
+    serializer_class = IngredientSerializer
+    def get_queryset(self):
+        return Ingredient.objects.all()
